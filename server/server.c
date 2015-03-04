e21668c7f89fa4a552b250170f13f5e17dea377c
Mike Goldin <skmgoldin@gmail.com>
2015-03-04 21:38:21 +0000

    Builds server.

--- server/server.c
+++ server/server.c
@@ -6,6 +6,18 @@ int main(int argc, char **argv) {
   int status;
   struct addrinfo hints;
   struct addrinfo *servinfo;
+
+  while(1) {
+  memset(&hints, 0, sizeof hints); 
+  hints.ai_family = AF_UNSPEC;     
+  hints.ai_socktype = SOCK_STREAM; 
+  hints.ai_flags = AI_PASSIVE;     
+
+  if((status = getaddrinfo(NULL, *(argv + 1), &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+  }
   //Wait for a client to connect
   //Send to login handler
+  }
 }
