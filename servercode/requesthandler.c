3fe4695e7371268b026eb3d2db8f33be90115097
Mike Goldin <skmgoldin@gmail.com>
2015-03-04 22:45:53 +0000

    This will be the main server loop, relies on servertools.

---
+++ servercode/requesthandler.c
@@ -0,0 +1,18 @@
+int main(int argc, char **argv) {
+
+  if(*(argv + 1) == NULL) {
+    printf("%s\n", "Run again with a port number, please.");
+    exit(1);
+  }
+
+  char *port = *(argv + 1);
+
+  int sock = makeserver(port); 
+
+  while(1) {
+
+    //Wait for a client to connect
+    //Send to login handler
+  }
+
+}
