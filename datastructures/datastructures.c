2563dd8ca589f8e86ed691dcf637039ac3245b16
Mike Goldin <skmgoldin@gmail.com>
2015-03-05 03:46:56 +0000

    Fixed malloc error.

--- datastructures/datastructures.c
+++ datastructures/datastructures.c
@@ -1,10 +1,13 @@
 #include "datastructures.h"
+#include <stdlib.h>
 
 struct candlemsg *alloccandlemsg() {
   struct candlemsg *candlemsg = malloc(sizeof(struct candlemsg));
-  candlemsg->versionid = malloc(sizeof(char * 32));
-  candlemsg->username = malloc(sizeof(char * 32));
+  candlemsg->versionid = malloc(sizeof(char) * 32);
+  candlemsg->clntid = malloc(sizeof(char) * 32);
   candlemsg->livestatus = malloc(sizeof(int));
-  candlemsg->reqtype = malloc(sizeof(char * 32));
-  candlemsg->msg = malloc(sizeof(char * 4096));
+  candlemsg->reqtype = malloc(sizeof(char) * 32);
+  candlemsg->msg = malloc(sizeof(char) * 4096);
+
+  return candlemsg;
 }
