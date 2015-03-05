bbd394cf409dad941f9f082bfd6c9c4af855159c
Mike Goldin <skmgoldin@gmail.com>
2015-03-05 01:48:20 +0000

    Forgot the semicolons.

--- datastructures/datastructures.h
+++ datastructures/datastructures.h
@@ -4,37 +4,37 @@
 struct clntreq {
   struct clnt *clnt = NULL;
   struct req *req = NULL;
-}
+};
 
 struct clnt {
   char *uid;
   int isonline;
-}
+};
 
 struct req {
   struct clntmsg *msg = NULL;
   struct clntlogin *login= NULL;
-}
+};
 
 struct clntmsg {
   struct clnt *from;
   struct clnt *to;
 
   char *msg;
-}
+};
 
 struct clntlogin {
   struct clnt *clnt = NULL;
   char *pw = NULL;
-}
+};
 
 struct liveusers {
   struct liveusernode *head;
-}
+};
 
 struct liveusernode {
   struct clnt *user;
   struct liveusernode *next;
-}
+};
 
 #endif
