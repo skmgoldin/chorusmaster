369424cf2acfbae16ee16624d0b94955817ad214
Mike Goldin <skmgoldin@gmail.com>
2015-03-05 04:41:24 +0000

    Error checking on input args.

--- clientcode/candleclient.c
+++ clientcode/candleclient.c
@@ -1,4 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char **argv) {
+
+  if(*(argv + 1) == NULL || *(argv + 2) == NULL) {
+    printf("%s\n", "Run again with an IP address and port number to connect "
+           "to.");
+    exit(1);
+  }
 
   char *ip = *(argv + 1);
   char *port = *(argv + 2);
