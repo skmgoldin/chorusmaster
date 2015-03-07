8d9b17da483de69419a41f9a15ef26236b7e5d02
Mike Goldin <skmgoldin@gmail.com>
2015-03-07 20:09:27 +0000

    Formatting.

--- sharedcode/datastructures.c
+++ sharedcode/datastructures.c
@@ -4,14 +4,19 @@
 
 struct candlemsg *alloccandlemsg(char *versionid, char *from) {
   struct candlemsg *candlemsg = malloc(sizeof(struct candlemsg));
+
   candlemsg->versionid = malloc(sizeof(char) * 32);
   strcpy(candlemsg->versionid, "candlechat 1.0");
+
   candlemsg->from = malloc(sizeof(char) * 32);
   *(candlemsg->from) = '\n';
+
   candlemsg->livestatus = malloc(sizeof(int));
   *(candlemsg->livestatus) = 0;
+
   candlemsg->reqtype = malloc(sizeof(char) * 32);
   *(candlemsg->reqtype) = '\n';
+
   candlemsg->msg = malloc(sizeof(char) * 4096);
   *(candlemsg->msg) = '\n';
 
