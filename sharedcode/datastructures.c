8bd2f63c7e8d2f5ef00bb7f9700b9f3c6ba6dc0e
Mike Goldin <skmgoldin@gmail.com>
2015-03-07 21:32:07 +0000

    Memsetting and globalvaluus.h complueance.

--- sharedcode/datastructures.c
+++ sharedcode/datastructures.c
@@ -1,22 +1,27 @@
 #include "datastructures.h"
 #include <stdlib.h>
 #include <string.h>
+#include "globalvalues.h"
 
 struct candlemsg *alloccandlemsg(char *versionid, char *from, char *reqtype,
                                  char *msg) {
 
   struct candlemsg *candlemsg = malloc(sizeof(struct candlemsg));
 
-  candlemsg->versionid = malloc(sizeof(char) * 32);
+  candlemsg->versionid = malloc(sizeof(char) * VERSIONIDLEN);
+  memset(candlemsg->versionid, 0, VERSIONIDLEN);
   strcpy(candlemsg->versionid, versionid);
 
-  candlemsg->from = malloc(sizeof(char) * 32);
+  candlemsg->from = malloc(sizeof(char) * FROMLEN);
+  memset(candlemsg->from, 0, FROMLEN);
   strcpy(candlemsg->from, from);
 
-  candlemsg->reqtype = malloc(sizeof(char) * 32);
+  candlemsg->reqtype = malloc(sizeof(char) * REQTYPELEN);
+  memset(candlemsg->reqtype, 0, REQTYPELEN);
   strcpy(candlemsg->reqtype, reqtype);
 
-  candlemsg->msg = malloc(sizeof(char) * 4096);
+  candlemsg->msg = malloc(sizeof(char) * MSGLEN);
+  memset(candlemsg->msg, 0, MSGLEN);
   strcpy(candlemsg->msg, msg);
 
   return candlemsg;
