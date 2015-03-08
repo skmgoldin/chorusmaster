b97dd95badf14e2b8f144c453170dc168eaf7496
Mike Goldin <skmgoldin@gmail.com>
2015-03-08 02:46:58 +0000

    Error checking for file opening.

--- servercode/authenticate.c
+++ servercode/authenticate.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include "../sharedcode/globalvalues.h"
+#include <stdlib.h>
 
 #define CREDFILE "credentials.txt"
 
 int authenticate(char *username, char *password) {
   
   FILE *credfile = fopen(CREDFILE, "r");
+  if(!credfile) {
+    perror("File opening failed");
+    exit(1);
+  }
 
   char *teststring;
   int i;
   for(i = 0; *(teststring = fgets(teststring, FROMLEN, credfile)) != EOF; i++) {
+    printf("%s\n", teststring);
     if(strcmp(username, teststring) && (i % 2) == 0) {
       if(strcmp(password, fgets(teststring, MSGLEN, credfile))) {
         return 1;
