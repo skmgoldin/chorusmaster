f50567f0a8195da8089f39cd9ae2cf3e1699e53f
Mike Goldin <skmgoldin@gmail.com>
2015-03-08 02:31:35 +0000

    Fixed compiler warnings and errors.

--- servercode/authenticate.c
+++ servercode/authenticate.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include "../sharedcode/globalvalues.h"
 
 #define CREDFILE "credentials.txt"
 
 int authenticate(char *username, char *password) {
   
-  FILE credfile = fopen(CREDFILE, "r");
+  FILE *credfile = fopen(CREDFILE, "r");
 
   char *teststring;
   int i;
-  for(i = 0; (teststring = fgets(credfile)) != EOF; i++) {
+  for(i = 0; *(teststring = fgets(teststring, FROMLEN, credfile)) != EOF; i++) {
     if(strcmp(username, teststring) && (i % 2) == 0) {
-      if(strcmp(password, fgets(credfile))) {
+      if(strcmp(password, fgets(teststring, MSGLEN, credfile))) {
         return 1;
       }
     }
