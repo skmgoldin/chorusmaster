97a6917f49652bd323950a6ae3f85cef76930060
Mike Goldin <skmgoldin@gmail.com>
2015-03-08 04:06:48 +0000

    Initial commit.

---
+++ servercode/userstatus.c
@@ -0,0 +1,5 @@
+#include "../sharedresources/datastructures.h"
+
+char *userlogin(char *username) {
+
+}
---
+++ servercode/userstatus.h
@@ -0,0 +1,7 @@
+#ifndef USERSTATUS_H
+#define USERSTATUS_H
+
+char *userlogin(char *username);
+
+#endif
+
