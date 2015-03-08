20147a3d76068668118b81d30b2496a724977b68
Mike Goldin <skmgoldin@gmail.com>
2015-03-08 01:59:22 +0000

    Initial commit.

---
+++ servercode/logger.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "../sharedcode/datastructures.h"
+#include "logger.h"
+
+struct candlemsg *candlelog(struct candlemsg *candlemsg) {
+  printf("%s%s%s%s\n", candlemsg->versionid, candlemsg->reqtype, candlemsg->from, candlemsg->msg);
+
+  return candlemsg;
+}
---
+++ servercode/logger.h
@@ -0,0 +1,6 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+struct candlemsg *candlelog(struct candlemsg *candlemsg);
+
+#endif
