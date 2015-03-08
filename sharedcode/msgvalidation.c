21c02cd3578b3c75858e0c3454b5f40441274eb8
Mike Goldin <skmgoldin@gmail.com>
2015-03-08 01:27:29 +0000

    Initial commit.

---
+++ sharedcode/msgvalidation.c
@@ -0,0 +1,20 @@
+#include "globalvalues.h"
+#include <string.h>
+#include <stdio.h>
+
+char *validatemsg(char *msg, int maxlen) {
+
+  int msglen = strlen(msg);
+
+  if(*(msg + msglen - 1) != '\n') {
+    if(msglen + 1 < maxlen) {
+      *(msg + (msglen)) = '\n';
+      *(msg + (msglen + 1)) = '\0';
+    }
+    else {
+      *(msg + msglen) = '\n';
+    }
+  }
+
+  return msg;
+}
---
+++ sharedcode/msgvalidation.h
@@ -0,0 +1,6 @@
+#ifndef MSGVALIDATION_H
+#define MSGVALIFATION_H
+
+char *validatemsg(char *msg, int maxlen);
+
+#endif
