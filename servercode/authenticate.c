c6791cfb9c7918ea51fa8198619c9e2b12ba477d
Mike Goldin <skmgoldin@gmail.com>
2015-03-08 02:24:14 +0000

    Reads from credentials.txt to determine whether a username/password combo is correct.

--- servercode/authenticate.c
+++ servercode/authenticate.c
@@ -1,9 +1,20 @@
+#include <stdio.h>
+
+#define CREDFILE "credentials.txt"
+
 int authenticate(char *username, char *password) {
   
+  FILE credfile = fopen(CREDFILE, "r");
+
+  char *teststring;
+  int i;
+  for(i = 0; (teststring = fgets(credfile)) != EOF; i++) {
+    if(strcmp(username, teststring) && (i % 2) == 0) {
+      if(strcmp(password, fgets(credfile))) {
+        return 1;
+      }
+    }
+  }
   
   return 0;
-  //Get uid.
-  //Check if user has logged in
-  //If yes, return 1
-  //If no, return 0.
 }
