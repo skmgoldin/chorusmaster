8f495465825c5940e23236927a499933729077b0
Mike Goldin <skmgoldin@gmail.com>
2015-03-08 19:50:36 +0000

    Better log formatting and logic.

--- servercode/logger.c
+++ servercode/logger.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 #include "../sharedcode/datastructures.h"
 #include "logger.h"
+#include "../sharedcode/globalvalues.h"
+#include <stdlib.h>
 
 struct candlemsg *candlelog(struct candlemsg *candlemsg) {
 
-  printf("%s%s%s%s\n", candlemsg->versionid, candlemsg->reqtype, candlemsg->from, candlemsg->msg);
+  serverlog("==MESSAGE==");
 
+  char *buf = malloc(sizeof(char) * MSGLEN * 2);
+
+  sprintf(buf, "%s%s", "versionid: ", candlemsg->versionid);
+  serverlog(buf);
+
+  sprintf(buf, "%s%s", "from: ", candlemsg->from);
+  serverlog(buf);
+
+  sprintf(buf, "%s%s", "reqtype: ", candlemsg->reqtype);
+  serverlog(buf);
+
+  sprintf(buf, "%s%s", "msg: ", candlemsg->msg);
+  serverlog(buf);
+
+  free(buf);
   return candlemsg;
 }
 
 char *serverlog(char *msg) {
 
-  printf("%s\n", msg);
+  printf("%s", msg);
 
   return msg;
 }
