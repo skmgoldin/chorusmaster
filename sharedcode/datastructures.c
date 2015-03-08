d10908f162ddbb267f7c0262f135b2531db39dfb
Mike Goldin <skmgoldin@gmail.com>
2015-03-08 01:28:44 +0000

    Now validates messages before packaging.

--- sharedcode/datastructures.c
+++ sharedcode/datastructures.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "globalvalues.h"
+#include "msgvalidation.h"
 
 struct candlemsg *alloccandlemsg(char *versionid, char *from, char *reqtype,
                                  char *msg) {
@@ -10,18 +11,22 @@ struct candlemsg *alloccandlemsg(char *versionid, char *from, char *reqtype,
 
   candlemsg->versionid = malloc(sizeof(char) * VERSIONIDLEN);
   memset(candlemsg->versionid, 0, VERSIONIDLEN);
+  versionid = validatemsg(versionid, VERSIONIDLEN);
   strcpy(candlemsg->versionid, versionid);
 
   candlemsg->from = malloc(sizeof(char) * FROMLEN);
   memset(candlemsg->from, 0, FROMLEN);
+  from = validatemsg(from, FROMLEN);
   strcpy(candlemsg->from, from);
 
   candlemsg->reqtype = malloc(sizeof(char) * REQTYPELEN);
   memset(candlemsg->reqtype, 0, REQTYPELEN);
+  reqtype = validatemsg(reqtype, REQTYPELEN);
   strcpy(candlemsg->reqtype, reqtype);
 
   candlemsg->msg = malloc(sizeof(char) * MSGLEN);
   memset(candlemsg->msg, 0, MSGLEN);
+  msg = validatemsg(msg, MSGLEN);
   strcpy(candlemsg->msg, msg);
 
   return candlemsg;
