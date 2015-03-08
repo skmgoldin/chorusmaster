6e344e33b33d71ccf10f2bce1aa0db8cd4cfae80
Mike Goldin <skmgoldin@gmail.com>
2015-03-08 05:40:47 +0000

    Alloc and init the live user list on server startup.

--- servercode/requesthandler.c
+++ servercode/requesthandler.c
@@ -23,6 +23,9 @@ int main(int argc, char **argv) {
   char *port = *(argv + 1);
   int sock = makeserver(port); 
 
+  struct liveuserlist *liveuserlist = malloc(sizeof(struct liveuserlist));
+  liveuserlist = initliveuserslist(liveuserlist);
+
   while(1) {
 
     int clntsock = getconnection(sock); // Update getconnection() to return struct sockdata.
