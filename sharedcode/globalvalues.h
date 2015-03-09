2c7b6f935b4586314fefb5d8284a4567d9657f94
Mike Goldin <skmgoldin@gmail.com>
2015-03-09 00:29:12 +0000

    Updated.

--- sharedcode/globalvalues.h
+++ sharedcode/globalvalues.h
@@ -2,9 +2,9 @@
 #define GLOBALVALUES_H
 
 #define VERSIONIDLEN 32
-#define FROMLEN 32
-#define LIVESTATUSLEN 1
 #define REQTYPELEN 32
+#define FROMLEN 32
+#define TOLEN 32
 #define MSGLEN 4096
 
 #define VERSIONID "candlechat1.0\n"
