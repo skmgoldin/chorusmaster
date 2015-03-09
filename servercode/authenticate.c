f5cb2f8bb7fed6518e59f7f2a9500bbbd8f49001
Mike Goldin <skmgoldin@gmail.com>
2015-03-09 01:54:23 +0000

    WIP for more robust login handling.

--- servercode/authenticate.c
+++ servercode/authenticate.c
@@ -5,24 +5,55 @@
 #include "../sharedcode/candlemsg.h"
 #include "../sharedcode/userlist.h"
 #include "authenticate.h"
+#include "../sharedcode/sockdata.h"
 
 #define CREDFILE "servercode/credentials.txt"
+#define TRIES 3
+#define LOCKOUT 60
 
-int authenticate(struct candlemsg *candlemsg, struct userlist *userlist) {
+int authenticate(struct candlemsg *candlemsg, struct userlist *userlist, 
+                 struct userlist *lockoutlist, struct sockdata *sockdata) {
 
   if(finduser(candlemsg->from, userlist) != NULL) {
+    /* User is already authenticated. */
+    // Check if sockdata is different!
     return 1;
   }
 
+  if(finduser(candlemsg->from, lockoutlist) != NULL) {
+    /* User is currently locked out. */
+    struct candlemsg *outmsg = alloccandlemsg();
+    char *msg = "You are locked out. Try again later.";
+    outmsg = packcandlemsg(outmsg, LOCKOUT, NULLFIELD, candlemsg->from, msg); 
+    sendcandlemsg(outmsg, sockdata->sock);
+    return 0;
+  }
+
   else if(strcmp(candlemsg->reqtype, LOGIN) == 0) { 
+    /* User is requesting to login. They get TRIES attempts. */
+
     if(login(candlemsg->from, candlemsg->msg)) {
-      adduser(candlemsg->from, userlist);
+      /* Login was successful, add user to userlist and authenticate. */
+      adduser(candlemsg->from, sockdata, userlist);
       return 1;
     }
 
+    /* First attempt failed, request try again. */
+    int i;
+    for(i = 0; i < TRIES - 1; i++) {
+      if(login(candlemsg->from, candlemsg->msg)) {
+        /* Login was successful, add user to userlist and authenticate. */
+        adduser(candlemsg->from, userlist);
+        return 1;
+      }
+    }
+
+    /* Login was not successful. User is added to lockout list. */
+    // Add user to lockout list.
     return 0;
   }
   
+  /* User is not authenticated */
   return 0;
 }
 
