abd5cbfcff277947c3b0f2d94d5fce7764178ae3
Mike Goldin <skmgoldin@gmail.com>
2015-03-10 08:26:09 +0000

    Initial commit.

---
+++ servercode/messagerouting.c
@@ -0,0 +1,23 @@
+#include "../sharedcode/userlist.h"
+#include "../sharedcode/wireio.h"
+#include "../sharedcode/candlemsg.h"
+#include "../sharedcode/globalvalues.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int broadcast(char *msg, struct userlist *to) {
+
+  struct candlemsg *candlemsg = alloccandlemsg();
+  candlemsg = packcandlemsg(candlemsg, MSG, NULLFIELD, NULLFIELD, NULLFIELD, msg);
+
+  struct usernode *currnode = to->head;
+  while(currnode != NULL) {
+    candleexchange(candlemsg, currnode->ip, currnode->port);
+    currnode = currnode->next;
+  }
+
+  free(msg);
+  dealloccandlemsg(candlemsg);
+
+  return 0;
+}
---
+++ servercode/messagerouting.h
@@ -0,0 +1,6 @@
+#ifndef MESSAGEROUTING_H
+#define MESSAGEROUTING_H
+
+int broadcast(char *msg, struct userlist *to);
+
+#endif
