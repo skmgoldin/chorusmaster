5f6b6fa99cbd9e469f37aea97d4ad025654b886c
Mike Goldin <skmgoldin@gmail.com>
2015-03-10 16:14:49 +0000

    Check for and remove newlines at the end of messages instead of adding them.

--- sharedcode/msgvalidation.c
+++ sharedcode/msgvalidation.c
@@ -6,15 +6,8 @@ char *validatemsg(char *msg, int maxlen) {
 
   int msglen = strlen(msg);
 
-  if(*(msg + msglen - 1) != '\n') {
-    if(msglen + 1 < maxlen) {
-      *(msg + (msglen)) = '\n';
-      *(msg + (msglen + 1)) = '\0';
-    }
-    else {
-      *(msg + msglen - 1) = '\n';
-      *(msg + msglen) = '\0';
-    }
+  if(*(msg + msglen - 1) == '\n') {
+    *(msg + msglen - 1) = '\0';
   }
 
   return msg;
