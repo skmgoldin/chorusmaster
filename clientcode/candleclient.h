1e05c5f897c48e55a4f39692ec779a54fad90c27
Mike Goldin <skmgoldin@gmail.com>
2015-03-18 23:10:58 +0000

    Some functions now take the pvtlist

--- clientcode/candleclient.h
+++ clientcode/candleclient.h
@@ -1,11 +1,13 @@
 #ifndef CANDLECLIENT_H
 #define CANDLECLIENT_H
 
+#include "../sharedcode/userlist.h"
+
 int login(char *servip, char *servport);
 
-int showrunner(char *servip, char *servport, char *mysock);
+int showrunner(char *servip, char *servport, char *mysock, struct userlist *pvtlist);
 
-int inputhandler(char *servip, char *servport);
+int inputhandler(char *servip, char *servport, struct userlist *pvtlist);
 
 char **parseinput(char *input);
 
